c978875520c189e35dc8d0220f8eefb41722cbf3
CalderWishne <calderw@gmail.com>
2014-04-27 14:15:42 -0500

    another debugging statement

--- app/controllers/state_records_controller.rb
+++ app/controllers/state_records_controller.rb
@@ -2,7 +2,8 @@ class StateRecordsController < ApplicationController
 
 	def get_record
 		puts "params[:state]: " + params[:state]
-		puts "in database: " + StateRecord.find_by(state: params[:state])
+		puts "in database: " 
+		p StateRecord.find_by(state: params[:state]).inspect
 		state_record = { 
 			state: params[:state], 
 			indices: StateRecord.find_by(state: params[:state]).indices
