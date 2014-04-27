5b8d4a5d95844c067e4aa290dca1c3b4565fde18
CalderWishne <calderw@gmail.com>
2014-04-27 14:18:53 -0500

    works on heroku again, so deleting debuggin statements

--- app/controllers/state_records_controller.rb
+++ app/controllers/state_records_controller.rb
@@ -1,9 +1,6 @@
 class StateRecordsController < ApplicationController
 
 	def get_record
-		puts "params[:state]: " + params[:state]
-		puts "in database: " 
-		p StateRecord.find_by(state: params[:state]).inspect
 		state_record = { 
 			state: params[:state], 
 			indices: StateRecord.find_by(state: params[:state]).indices
