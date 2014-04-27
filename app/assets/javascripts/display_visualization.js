f8c9095efa0ff7879c55d263b57ee6e89ad7a8d3
CalderWishne <calderw@gmail.com>
2014-04-27 04:25:37 -0500

    adds color change feature

--- app/assets/javascripts/display_visualization.js
+++ app/assets/javascripts/display_visualization.js
@@ -4,15 +4,17 @@ $(function() {
   $('select').on('change', function(event) {
   	event.preventDefault();
   	var yearString = $('#year').val();
-  	var color = $('#color').val();
+  	var low_color = $('#low_color').val();
+  	var high_color = $('#high_color').val();
   	$.post('/indices/min_max', {year: yearString}, function(data){
-  		choropleth.colorStates(yearString, parseFloat(data[0]), parseFloat(data[1]), color);
+  		choropleth.colorStates(yearString, parseFloat(data[0]), parseFloat(data[1]), low_color, high_color);
   	});	
   });
 
   $('.states').on('click', 'path', function() {
-  		alert($(this).attr('id'));
-  	}); 
+  	alert($(this).attr('id'));
+  }); 
+  // new LineChart("state");
 });
 
 
